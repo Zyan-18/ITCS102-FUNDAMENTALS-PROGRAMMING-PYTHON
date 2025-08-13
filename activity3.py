3208b915712baa24fb9a75612922a69510c0669d
Zyan-18 <sanchezzyankloe@gmail.com>
2025-08-13 10:50:31 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,2 @@
+print("The quick\n brown fox\t jumps over the lazy dog.")
+print("\"Ang kulit mo 'no?\"")
---
+++ code_challenge1.py
@@ -0,0 +1,2 @@
+name=input()
+print("\t\t\t\t\t*\n\n\n"   "\t\t\t\t*\t\t*\n\n\n"    "\t\t\t*\t\t\t\t*\n\n\n"    "\t\t*\t\t\t\t\t\t*\n\n\n"    "\t*\t\t\t\t" "Hi,",name,"\t\t\t*\n\n\n"      "\t\t*\t\t\t\t\t\t*\n\n\n"      "\t\t\t*\t\t\t\t*\n\n\n"    "\t\t\t\t*\t\t*\n\n\n"    "\t\t\t\t\t*\n\n")
