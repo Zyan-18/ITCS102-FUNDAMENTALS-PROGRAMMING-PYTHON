9b16870279dddce720d138b0a2bf5c8e24f59ea8
Zyan-18 <sanchezzyankloe@gmail.com>
2025-08-16 16:49:42 +0800

    Add files via upload

---
+++ activity5.py
@@ -0,0 +1,4 @@
+something =eval (input("input something -->"))
+print("The data type of something is", type (something))
+answer = 10 + something
+print("the answer is ", answer)
---
+++ activity6.py
@@ -0,0 +1,15 @@
+a1=eval(input("Enter the first number : "))
+a2=eval(input("Enter the second number : "))
+s=a1+a2
+d=a1-a2
+p=a1*a2
+q=a1/a2
+
+#solution=))a1/a2)*100-5)//300
+print("\nThe sum of" ,a1, "and" ,a2, "is" ,s)
+print("The difference of" ,a1, "and" ,a2, "is" ,d)
+print("The product of" ,a1, "and" ,a2, "is" ,p)
+print("The quotient of" ,a1, "and" ,a2, "is" ,q)
+print(a1, "exponent by" ,a2, "is" ,a1**a2)
+print("The remainder of" ,a1, "and" ,a2, "is" ,a1 % a2)
+print("The floor division of" ,a1, "and" ,a2, "is" ,a1//a2)
