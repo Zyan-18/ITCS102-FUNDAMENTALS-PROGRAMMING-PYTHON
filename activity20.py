b66336db6d38f6236ef9093b351b24f7a1f4a076
Zyan-18 <sanchezzyankloe@gmail.com>
2025-09-22 11:29:19 +0800

    Add files via upload

---
+++ activity20.py
@@ -0,0 +1,6 @@
+for z in range(1, 11, 1):
+    for y in range(1, z, 1):
+        print(" ", end=" ")
+    for a in range(10, z, -1):
+        print(a, end=" ")
+    print() 
---
+++ code_challenge10.py
@@ -0,0 +1,8 @@
+for z in range(1, 11, 1):
+    for y in range(10, z, -1):
+        print(" ", end=" ")
+    for a in range(1, z, 1):
+        print("*", end=" ")
+    for n in range(1, z, 1):
+        print("*", end=" ")
+    print() 
