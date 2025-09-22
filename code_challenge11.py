b9cf74def19d30e95161eacce59ea28b0f2e2f00
Zyan-18 <sanchezzyankloe@gmail.com>
2025-09-22 16:19:31 +0800

    Add files via upload

---
+++ code_challenge11.py
@@ -0,0 +1,19 @@
+#Diamond Asteries
+
+for z in range(1,11,1):
+    for y in range(10,z,-1):
+        print(" ", end=" ")
+    for a in range(1,z,1):
+        print("*", end=" ")
+    for a in range(1,z,1):
+        print("*", end=" ")
+    print()
+    
+for z in range(1,11,1):
+    for n in range(0,z,1):
+        print(" ", end=" ")
+    for a in range(9,z,-1):
+        print("*", end=" ")
+    for a in range(9,z,-1):
+        print("*", end=" ")
+    print()
