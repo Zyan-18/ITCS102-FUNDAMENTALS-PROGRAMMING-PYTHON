0c74ef6eb84a47dabbd1a0dd1879affad2028b46
Zyan-18 <sanchezzyankloe@gmail.com>
2025-09-22 16:08:21 +0800

    Update activity20.py

--- activity20.py
+++ activity20.py
@@ -1,6 +1,6 @@
-for z in range(1, 11, 1):
-    for y in range(1, z, 1):
+for z in range(0, 11, 1):
+    for y in range(0, z, 1):
         print(" ", end=" ")
     for a in range(10, z, -1):
         print(a, end=" ")
-    print() 
+    print()
