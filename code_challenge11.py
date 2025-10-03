e0a4205f07cc7a91ff06be0ed4a24b6a6c38df3c
Zyan-18 <sanchezzyankloe@gmail.com>
2025-10-03 10:45:30 +0800

    Update code_challenge11.py

--- code_challenge11.py
+++ code_challenge11.py
@@ -5,18 +5,19 @@ for z in range(1,11,1):
         print(" ", end=" ")
     for a in range(1,z,1):
         print("*", end=" ")
-    for a in range(1,z,1):
+    for n in range(2,z,1):
         print("*", end=" ")
     print()
     
 for z in range(1,11,1):
-    for n in range(0,z,1):
+    for y in range(0,z,1):
         print(" ", end=" ")
     for a in range(9,z,-1):
         print("*", end=" ")
-    for a in range(9,z,-1):
+    for n in range(8,z,-1):
         print("*", end=" ")
     print()
+
 
 
 
