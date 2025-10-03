ceccd8b29e33f7b0286cd58c07c30bfc0ed636a2
Zyan-18 <sanchezzyankloe@gmail.com>
2025-10-03 10:57:27 +0800

    Add files via upload

---
+++ code_challenge13.py
@@ -0,0 +1,48 @@
+for z in range(1,6,1):
+    for y in range(24,z,-1):
+        print(" ", end=" ")
+    for a in range(1,z,1):
+        print("*", end=" ")
+    for n in range(2,z,1):
+        print("*", end=" ")
+    print()
+for z in range(6,1,-1):
+    for y in range(24,z,-1):
+        print(" ", end=" ")
+    for a in range(1,z,1):
+        print("*", end=" ")
+    for n in range(2,z,1):
+        print("*", end=" ")
+    print()
+for z in range(3,15,1):
+    for y in range(24,z,-1):
+        print(" ", end=" ")
+    for a in range(1,z,1):
+        print("*", end=" ")
+    for n in range(2,z,1):
+        print("*", end=" ")
+    print()
+for z in range(2,20,1):
+    for y in range(24,z,-1):
+        print(" ", end=" ")
+    for a in range(1,z,1):
+        print("*", end=" ")
+    for n in range(2,z,1):
+        print("*", end=" ")
+    print()
+for z in range(2,25,1):
+    for y in range(24,z,-1):
+        print(" ", end=" ")
+    for a in range(1,z,1):
+        print("*", end=" ")
+    for n in range(2,z,1):
+        print("*", end=" ")
+    print()
+for z in range(14,21,1):
+    for y in range(20,z,-1):
+        print(" ", end=" ")
+    for a in range(1,z,1):
+        print(" ", end=" ")
+    for n in range(0,7,1):
+        print("*", end=" ")
+    print()
