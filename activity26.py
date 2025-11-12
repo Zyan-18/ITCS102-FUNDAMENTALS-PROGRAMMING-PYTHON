a2017dcccf942bf13845445bbf2c9e3fb2a55666
Zyan-18 <sanchezzyankloe@gmail.com>
2025-11-12 16:38:56 +0800

    Add files via upload

---
+++ activity26.py
@@ -0,0 +1,7 @@
+#dictionary
+
+programming_language = ['Python', 'C++', 'Java', 'JavaScript', 'Perl', 'Ruby']
+
+prog_lang = {'py':'Python','C':'C++','J':'Java','JV':'JavaScript','P':'Perl','R':'Ruby'}
+
+print(prog_lang['py'])
