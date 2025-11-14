50db9c0b7809242179fc960e5e0e42d18ff0cc7c
Zyan-18 <sanchezzyankloe@gmail.com>
2025-11-14 21:21:23 +0800

    Update activity25.py

--- activity25.py
+++ activity25.py
@@ -1,4 +1,5 @@
 from activity25_1 import *
+from activity25_2 import *
 
 name = input("Hello! Please enter your name: ")
 
@@ -8,25 +9,56 @@ isContinue = True
 
 while isContinue == True:
     print("Please select a program.")
-    print(" a - activity12\n b - activity13\n c - activity14\n d - activity15\n e - Exit the compiler program")
+    folder = input("Choose the folder that you wanted to open.\n\t Enter f1 for folder1 and f2 for for folder2.\n\t Input here: ").lower()
 
-    choose = input("Please enter the chosen program: ")
+    if folder == "f1":
+        print(" a - activity11\n b - activity12\n c - activity13\n d - activity14\n e - activity15\n f - Exit the compiler program")
 
-    if choose == "a":
-        activity12()
-        continue
-    elif choose == "b":
-        activity13()
-        continue
-    elif choose == "c":
-        activity14()
-        continue
-    elif choose == "d":
-        activity15()
-        continue
-    elif choose == "e":
-        print("You have exited the program. Thank you!")
-        break
+        choose = input("Please enter the chosen program: ")
+
+        if choose == "a":
+            activity11()
+            continue
+        elif choose == "b":
+            activity12()
+            continue
+        elif choose == "c":
+            activity13()
+            continue
+        elif choose == "d":
+            activity14()
+            continue
+        elif choose == "e":
+            activity15()
+            continue
+        elif choose == "f":
+            print("You have exited the program. Thank you!")
+            break
+        else:
+            print("Invalid input. Please try again.")
+            continue
+    elif folder == "f2":
+        print(" a - activity16\n b - activity17\n c - activity18\n d - activity19\n e - activity20\n f - Exit the compiler program")
+        
+        choose = input("Please enter the chosen program: ")
+
+        if choose == "a":
+            activity16()
+            continue
+        elif choose == "b":
+            activity17()
+            continue
+        elif choose == "c":
+            activity18()
+            continue
+        elif choose == "d":
+            activity19()
+            continue
+        elif choose == "e":
+            activity20()
+            continue
+        else:
+            print("Invalid input. Please try again.")
+            continue
     else:
         print("Invalid input. Please try again.")
-        continue
